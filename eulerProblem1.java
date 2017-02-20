f38366cabf6b8f865f6e706a8875258d85599893
Pandey270491 <shraddhapandeysh@gmail.com>
2017-02-20 13:05:26 +0530

    Update eulerProblem1.java

--- eulerProblem1.java
+++ eulerProblem1.java
@@ -2,19 +2,21 @@
  * Created by pandey on 10/02/17.
  */
 public class eulerProblem1 {
-    public static void main(String[] args) {
+   public static void main(String[] args)
+    {
         CheckAndAdd();
     }
 
     public static void CheckAndAdd() {
-        long total = 0L;
+        long total=0;
 
-        for(int i = 1; i < 1000; ++i) {
-            if(i % 3 == 0 || i % 5 == 0) {
-                total += (long)i;
+        for( int i=1 ; i<1000 ; i ++) {
+            if(i%3==0 || i%5==0)
+            {
+                total +=i;
             }
-        }
 
+        }
         System.out.println(total);
     }
 }
