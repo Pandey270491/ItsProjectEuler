beb4856c2aa5c2f19ff64af763fa3a91cb98f6a1
Pandey270491 <shraddhapandeysh@gmail.com>
2017-02-20 13:07:50 +0530

    Update eulerProblem4.java

--- eulerProblem4.java
+++ eulerProblem4.java
@@ -2,39 +2,59 @@
  * Created by pandey on 10/02/17.
  */
 public class eulerProblem4 {
-    public static void main(String[] args) {
-        int num1 = 999;
-        boolean num2 = false;
-        int LargestNum1 = 0;
-        int LargetsNum2 = 0;
-        int LargestPallendrome = 0;
+   public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        int num1=999;
+        int num2=0;
+        int LargestNum1=0;
+        int LargetsNum2=0;
+        int LargestPallendrome=0;
+        boolean pFound=false;
 
-        for(boolean pFound = false; num1 >= 100; --num1) {
-            for(int var8 = 999; var8 >= 100; --var8) {
-                if(checkPallindrome(num1, var8)) {
-                    int pNumber = num1 * var8;
-                    if(pNumber > LargestPallendrome) {
-                        LargestPallendrome = pNumber;
-                        LargestNum1 = num1;
-                        LargetsNum2 = var8;
+        while(num1>=100)
+        {
+            num2=999;
+            while(num2>=100) {
+
+                if (checkPallindrome(num1, num2)) {
+                  int pNumber=num1*num2;
+                    if(pNumber>LargestPallendrome)
+                    {
+                        LargestPallendrome=pNumber;
+                        LargestNum1=num1;
+                        LargetsNum2=num2;
                     }
                 }
+                num2--;
             }
+            num1--;
         }
+        System.out.println("largest palindrome " + LargestPallendrome + "= " +LargestNum1 + "*"+ LargetsNum2);
+        long endTime = System.currentTimeMillis();
+        System.out.println("That took " + (endTime - startTime) + " milliseconds");
 
-        System.out.println("largest palindrome " + LargestPallendrome + "= " + LargestNum1 + "*" + LargetsNum2);
     }
 
-    public static boolean checkPallindrome(int n1, int n2) {
-        int number = n1 * n2;
-        int constructReverse = 0;
+    public static boolean checkPallindrome(int n1, int n2)
+    {
+        int number=n1*n2;
+        int constructReverse =0;
+        int digit=0;
 
-        for(boolean digit = false; number > 0; number /= 10) {
-            int digit1 = number % 10;
-            constructReverse = constructReverse * 10 + digit1;
+        while(number>0)
+        {
+            digit=number%10;
+            constructReverse = constructReverse *10 + digit;
+            number = number/10;
+        }
+        constructReverse = constructReverse +number;
+
+        if(constructReverse==n1*n2)
+        {
+            return true;
         }
 
-        constructReverse += number;
-        return constructReverse == n1 * n2;
+        return false;
+
     }
 }
